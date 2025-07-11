fd8dbd0f1d047b81907103ebbef4c63a70d3c01f
Eyobed <eyobedteshome@gmail.com>
2025-07-12 02:57:42 +0300

    reusable context manager databaseExecution created

---
+++ python-context-async-perations-0x02/1-execute.py
@@ -0,0 +1,21 @@
+import sqlite3
+
+class DatabaseExecution:
+    def __init__(self, db, query, param):
+        self.db = db
+        self.query = query
+        self.param = param
+        
+    def __enter__(self):
+        self.conn = sqlite3.connect(self.db)
+        cursor = self.conn.cursor() 
+        cursor.execute(self.query, (self.param,)) 
+        results = cursor.fetchall()
+        return results
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        self.conn.close()
+    
+
+with DatabaseExecution("users.db","SELECT * FROM users WHERE age > ?", 25) as result: 
+    print(result)
