1b84640a87224020fbb258d09f4095465bf0deb2
Eyobed <eyobedteshome@gmail.com>
2025-07-12 01:28:33 +0300

    transactional wrapper created

---
+++ python-decorators-0x01/2-transactional.py
@@ -0,0 +1,45 @@
+import sqlite3 
+import functools
+
+def with_db_connection(func):
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        conn = sqlite3.connect("users.db")
+        try:
+            return func(conn=conn, *args, **kwargs)
+        finally:
+            conn.close()
+
+    return wrapper
+
+# transactional decorator that ensures a function running a database operation is wrapped inside a transaction
+def transactional(func):
+    @functools.wraps(func)
+    def wrapper(conn, *args, **kwargs):
+        try:
+            result = func(conn, *args, **kwargs)
+            conn.commit()
+            return result
+        except Exception as e:
+            conn.rollback()
+            raise
+    return wrapper
+
+@with_db_connection 
+def get_user_by_id(conn, user_id): 
+    cursor = conn.cursor() 
+    cursor.execute("SELECT * FROM users WHERE id = ?", (user_id,)) 
+    return cursor.fetchone() 
+#### Fetch user by ID with automatic connection handling 
+
+user = get_user_by_id(user_id=1)
+print(user)
+
+@with_db_connection 
+@transactional 
+def update_user_email(conn, user_id, new_email): 
+    cursor = conn.cursor() 
+    cursor.execute("UPDATE users SET email = ? WHERE id = ?", (new_email, user_id)) 
+#### Update user's email with automatic transaction handling 
+
+update_user_email(user_id=1, new_email='Crawford_Cartwright@hotmail.com')
