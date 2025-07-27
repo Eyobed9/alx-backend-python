b17a6ad8869f83d2b61333036a781de150122f68
Eyobed <eyobedteshome@gmail.com>
2025-07-27 23:58:37 +0300

    message app created

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # "rest_framework_simplejwt", "DEFAULT_AUTHENTICATION_CLASSES"]
+    # ["rest_framework.authentication.BasicAuthentication", "rest_framework.authentication.SessionAuthentication"]
 ]
 
 MIDDLEWARE = [
